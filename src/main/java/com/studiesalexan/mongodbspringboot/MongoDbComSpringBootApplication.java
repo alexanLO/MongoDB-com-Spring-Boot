9a52f6d6cbf666c3682bf82970b14fd7e7ee6f0e
Alexan Lima <alexanlima1.0@gmail.com>
2022-05-23 14:47:02 -0300

    Primeiro commit: projeto criado

---
+++ src/main/java/com/studiesalexan/mongodbspringboot/MongoDbComSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.studiesalexan.mongodbspringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MongoDbComSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MongoDbComSpringBootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/studiesalexan/mongodbspringboot/MongoDbComSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.studiesalexan.mongodbspringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MongoDbComSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
